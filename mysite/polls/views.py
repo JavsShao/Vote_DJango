70aa8fab78824c8074b8aea8d67ce061cd4ae6c1
JavsShao <javs_shao@163.com>
2019-03-19 16:19:40 +0800

    views视图_1_编写详情、结果、以及投票id

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, World! You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(requst, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response, question_id)
+
+def vore(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
