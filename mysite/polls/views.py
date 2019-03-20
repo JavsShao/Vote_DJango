a3be7b14486a652ac73690fee3ccff6b8ef39093
JavsShao <javs_shao@163.com>
2019-03-20 14:38:26 +0800

    选择投票_后端

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, response, Http404
+from django.http import HttpResponse, response, Http404, HttpResponseRedirect
 
 # Create your views here.
 from django.template import loader
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -30,5 +31,31 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
+    '''
+    查看投票结果
+    :param request:
+    :param question_id:
+    :return:
+    '''
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question':question})
+
+def vote(request, question_id):
+    '''
+    选择投票
+    :param request:
+    :param question_id:
+    :return:
+    '''
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExists):
+        return render(request, 'polls/detail.html', {
+            'question':p,
+            'error_message':"你没有选择！"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id)))
