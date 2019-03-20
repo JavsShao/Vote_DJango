bbb7753b9aef29737c4d7e84a2ea8d73970ef074
JavsShao <javs_shao@163.com>
2019-03-20 09:57:55 +0800

    删除模板中的硬编码URL

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, response
-
+from django.http import HttpResponse, response, Http404
 
 # Create your views here.
 from django.template import loader
@@ -19,7 +18,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(requst, question_id):
     response = "You're looking at the results of question %s."
