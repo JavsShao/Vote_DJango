9df9e9e632e6828098cf770b4518c468127dcccc
JavsShao <javs_shao@163.com>
2019-03-20 15:28:16 +0800

    解决投票模块出现的问题

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -41,21 +41,19 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    '''
-    选择投票
-    :param request:
-    :param question_id:
-    :return:
-    '''
     p = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExists):
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
-            'question':p,
-            'error_message':"你没有选择！"
+            'question': p,
+            'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id)))
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
